fd3b99589323023fe7abc2b7d88d681ec99f9a15
CRISSIS <youglive@gmail.com>
2021-10-11 22:38:46 -0600

    Generación de Modelos Bases
    Se generaron y adminsitraron los modelos bases para controlar la
    información de los distintos elementos del software en desarrollo

--- GestorVentas2017009215/Apps/GestorVentas/admin.py
+++ GestorVentas2017009215/Apps/GestorVentas/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 #importamos los modelos
-from .models import Prueba
+from .models import *
 
 # Register your models here.
 
@@ -9,3 +9,6 @@ Agregamos nuestros modelos para poder
 configurarlos en el panel admin
 """
 admin.site.register(Prueba)
+admin.site.register(Roles)
+admin.site.register(Acciones)
+admin.site.register(Bitacoras)
--- GestorVentas2017009215/Apps/GestorVentas/models.py
+++ GestorVentas2017009215/Apps/GestorVentas/models.py
@@ -7,6 +7,16 @@ class Prueba(models.Model):
     nombre = models.CharField(max_length=50)
 
 
-class Roles (models.Model):
+class Roles(models.Model):
     ID_Rol = models.CharField(primary_key=True, max_length=5)
     nombre = models.CharField(max_length=50)
+
+class Acciones(models.Model):
+    ID_Rol = models.CharField(primary_key=True, max_length=5)
+    nombre = models.CharField(max_length=50)
+
+class Bitacoras(models.Model):
+    ID_Bitacora = models.CharField(primary_key=True, max_length=5)
+    fecha = models.CharField(max_length=50)
+    hora = models.CharField(max_length=50)
+    direccion_IP = models.CharField(max_length=50)
